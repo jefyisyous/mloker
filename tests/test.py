484f01463585d6d34eb0f509f20b8d4b84368f52
jeffy <jeffy.junfu@gmail.com>
2015-07-04 17:03:40 +0800

    init

---
+++ tests/test.py
@@ -0,0 +1,20 @@
+#coding:utf8
+
+"""
+Created on 2015-07-04
+
+@author: junfu
+@description:
+         
+Copyright (c) 2014 infohold inc. All rights reserved.
+"""
+from app.lean.sdk import Users
+import simplejson
+
+if __name__ == '__main__':
+    user = Users.get()
+    print type(user)
+    print user
+    user2 = simplejson.dumps(user)
+    print type(user2)
+    print user2
