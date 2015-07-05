43cb933ab7c1ae9d25fcd65dcf7c70fc7f83a3d6
jeffy <jeffy.junfu@gmail.com>
2015-07-05 17:45:43 +0800

    init

--- portal/main/views.py
+++ portal/main/views.py
@@ -14,6 +14,7 @@ from flask import render_template, redirect, url_for, abort, flash, request
 from . import main
 
 from portal.api_1_0.users import Users
+import simplejson
 
 
 # @main.route('/user/<username>')
@@ -36,6 +37,6 @@ def index():
 
 @main.route('/users', methods=['GET'])
 def users():
-    user = Users.get()["results"]
+    users = Users.get()["results"]
     #page = request.args.get('page', 1, type=int)
-    return render_template('user.html', user=user)
+    return render_template('user.html', users=simplejson.dumps(users), posts=10, pagination=10)
--- tests/test.py
+++ tests/test.py
@@ -8,13 +8,20 @@ Created on 2015-07-04
          
 Copyright (c) 2014 infohold inc. All rights reserved.
 """
-from portal.lean.sdk import Users
+from portal.api_1_0.users import Users
+from portal.app import configure_leancloud
 import simplejson
+import leancloud
 
+
+#
 # if __name__ == '__main__':
+#     leancloud.init('mqoay5ghtispkaxpeo4eety4t07pmn3kpahecoa7frg981v6',
+#                    app_key='7iei7k6mg98ssp69056u35bbevpxzgplt0iyifsrpbrtj5xo',
+#                    master_key='j8qpixm9lyhhvuzlc2zyihs9ba17fkhdre3bvi7z6apfjo67')
 #     user = Users.get()
 #     print type(user)
 #     print user
-#     user2 = simplejson.dumps(user)
-#     print type(user2)
-#     print user2
+#     print user.get("results")
+#     print simplejson.dumps(user.get("results"))
+
